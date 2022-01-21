a784ebed72a58c033084fd592ed552511494345b
Evan Conrad <evan@roomservice.dev>
2022-01-21 10:52:55 -0800

    rm console.log

--- apps/web/pages/api/graphql.tsx
+++ apps/web/pages/api/graphql.tsx
@@ -10,7 +10,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log("Open metagraph");
   let schemas = req.headers["x-omg-schemas"];
   if (!schemas || schemas.length === 0) {
     schemas = [];
