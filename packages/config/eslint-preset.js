1b32983a5a54fca41eff308af176d4dec997b7bb
Evan Conrad <evan@roomservice.dev>
2022-01-21 14:40:45 -0800

    goodnight eslint

--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -7,5 +7,10 @@ module.exports = {
   },
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    "react/no-unescaped-entities": "off",
+    "react/no-unknown-property": "off",
+    "@next/next/no-page-custom-font": "off",
+    "@next/next/no-html-link-for-pages": "off",
+    "react/jsx-no-target-blank": "off",
   },
 };
