cd6a6278b457df1502e07858f4ca0d5ffbd29527
Jacob Peddicord <jacob.peddicord@hey.com>
2022-01-24 13:52:13 -0500

    add validation to create document

--- packages/graphql/buildCreateDocument.ts
+++ packages/graphql/buildCreateDocument.ts
@@ -4,6 +4,7 @@ import { pick, type } from "superstruct";
 import { assertOrThrow, ValidOpenMetaGraphDocument } from "./validation";
 import { CreateResponse, DocumentInputType } from "./fields";
 import { Hooks } from "./types";
+import { OpenMetaGraphSchema } from "openmetagraph";
 
 export function buildCreateDocument(hooks: Hooks) {
   return {
@@ -19,21 +20,46 @@ export function buildCreateDocument(hooks: Hooks) {
         document,
         pick(ValidOpenMetaGraphDocument, ["schemas", "elements"])
       );
+
+      var validSchemaElements: { key: string; object: "string" | "number" | "file" | "node"; multiple: boolean; }[] = [];
+      for (const schemaCID of document.schemas) {
+        const schema = await hooks.onGetResource(schemaCID) as OpenMetaGraphSchema;
+        Object.keys(schema.elements).forEach((key: string) => {
+          validSchemaElements.push({
+            "key": key,
+            object: schema.elements[key].object,
+            multiple: schema.elements[key].multiple,
+          })
+        });
+      }
+      var missingSchemaElements = [...validSchemaElements];
       document.elements.forEach((e: any) => {
         if (!e) throw new GraphQLError("Empty elements are not allowed");
-        const obj = e.object;
-        if (
-          !(
-            obj === "number" ||
-            obj === "string" ||
-            obj === "file" ||
-            obj === "node"
-          )
-        ) {
-          return new GraphQLError(`Unexpected object '${e.object}'`);
-        }
+
+        // Check that the key is valid
+        const schemaElement = validSchemaElements.find(el => el.key === e.key);
+        if(!schemaElement) {
+          throw new GraphQLError(`Key '${e.key}' is not a valid key. It is either not in the provided schema or is an invalid multiple.`);
+        } 
+        // Remove the element from missing elements
+        missingSchemaElements = missingSchemaElements.filter(elm => elm.key !== e.key);
+        
+        // Check that the element is the proper type
+        if (schemaElement.object !== e.object)
+          throw new GraphQLError(`Invalid object '${e.object}' for key '${e.key}', expected '${schemaElement.object}'`);
+
+        // If multiple is false, the element cannot be in the schema again
+        if (!schemaElement.multiple) {
+          validSchemaElements = validSchemaElements.filter(elm => elm.key !== e.key);
+        } 
+
         return null;
       });
+
+      // Check that all required elements were provided
+      if (missingSchemaElements.length > 0) {
+        throw new GraphQLError(`The following elements must be provided:${missingSchemaElements.map(el => ` ${el.key} (${el.object})`)}`);
+      }
 
       const result = await hooks.onPostDocument({
         object: "omg",
