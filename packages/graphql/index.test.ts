6f849f5f751ebd86c931f3e142c66f5ef8c41cae
Jacob Peddicord <jacob.peddicord@hey.com>
2022-01-24 13:53:27 -0500

    fix tests

--- packages/graphql/index.test.ts
+++ packages/graphql/index.test.ts
@@ -125,6 +125,12 @@ test("CreateDocument example", async () => {
             object: "string",
             value: "hello world",
           },
+          {
+            key: "photos",
+            object: "file",
+            contentType: "img",
+            uri: "testuri"
+          },
         ],
         schemas: ["schema"],
       },
