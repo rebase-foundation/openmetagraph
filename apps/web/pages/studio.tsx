3ea9616bd1dfd14bb127a4c997783ed9ab88f8f0
Evan Conrad <evan@roomservice.dev>
2022-03-07 09:06:28 -0800

    fix confusion on string types

--- apps/web/pages/studio.tsx
+++ apps/web/pages/studio.tsx
@@ -112,8 +112,66 @@ export default function Web(props) {
         className="flex text-sm items-center border-b flex-row w-full h-full font-mono bg-gray-50"
         key={"el" + el.key}
       >
-        <div className="px-2 border-r justify-center w-24 bg-gray-50 flex items-center h-full">
-          {el.object}
+        <div
+          className={cn({
+            "px-2 border-r justify-between items-center w-24 flex h-full": true,
+            "text-green-800 bg-green-50": el.object === "node",
+            "text-blue-800 bg-blue-50": el.object === "file",
+            "text-red-800 bg-red-50": el.object === "number",
+            "text-purple-800 bg-purple-50": el.object === "string",
+          })}
+        >
+          <div>{el.object}</div>
+          {el.object === "file" && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-3 w-3"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z"
+              />
+            </svg>
+          )}
+
+          {el.object === "node" && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-3 w-3"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+              />
+            </svg>
+          )}
+
+          {el.object === "number" && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-3 w-3"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M7 20l4-16m2 16l4-16M6 9h14M4 15h14"
+              />
+            </svg>
+          )}
         </div>
 
         <div className="bg-gray-50 flex flex-1 items-center px-2 border-r h-full">
@@ -178,14 +236,41 @@ export default function Web(props) {
   return (
     <div className="bg-gray-50 flex w-full">
       <div className="flex flex-col bg-white w-full h-full max-w-6xl mx-auto border-l border-r">
-        <div className="px-2 py-4 border-b items-center flex text-sm  text-gray-400">
+        <div className="px-2 py-4 border-b items-center flex justify-between text-sm  text-gray-400">
           <div className="mr-4">OpenMetaGraph Studio</div>
-          <input
+          <div
             placeholder="ipfs://..."
-            value={("ipfs://" + router.query.schema) as any}
             onChange={() => {}}
-            className="flex border bg-white p-2 flex-1"
-          />
+            className="bg-gray-50  flex items-center"
+          >
+            <div className="py-2 px-2">
+              {(router.query.schema as any) || "..."}
+            </div>
+            <a
+              className="border-l py-2 pl-2 px-2 hover:bg-gray-200"
+              href={
+                router.query.schema
+                  ? `/studio?schema=${router.query.schema}`
+                  : "/"
+              }
+              target="_blank"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+                />
+              </svg>
+            </a>
+          </div>
         </div>
 
         <div className="px-2 py-1 border-b flex justify-between text-sm bg-gray-100 text-gray-400">
@@ -204,9 +289,9 @@ export default function Web(props) {
               <button
                 className={cn({
                   "border hover:opacity-70 px-4 py-2 flex-1": true,
-                  "bg-blue-500 text-white border-blue-900 shadow-inner":
+                  "bg-purple-500 text-white border-purple-900 shadow-inner":
                     next.object === "string",
-                  "bg-white": next.object !== "string",
+                  "bg-white text-purple-600": next.object !== "string",
                 })}
                 onClick={() => setNext((p) => ({ ...p, object: "string" }))}
               >
@@ -215,9 +300,9 @@ export default function Web(props) {
               <button
                 className={cn({
                   "border hover:opacity-70 px-4 py-2 flex-1": true,
-                  "bg-blue-500 text-white border-blue-900 shadow-inner":
+                  "bg-red-500 text-white border-red-900 shadow-inner":
                     next.object === "number",
-                  "bg-white": next.object !== "number",
+                  "bg-white text-red-600": next.object !== "number",
                 })}
                 onClick={() => setNext((p) => ({ ...p, object: "number" }))}
               >
@@ -228,7 +313,7 @@ export default function Web(props) {
                   "border hover:opacity-70 px-4 py-2 flex-1": true,
                   "bg-blue-500 text-white border-blue-900 shadow-inner":
                     next.object === "file",
-                  "bg-white": next.object !== "file",
+                  "bg-white text-blue-600": next.object !== "file",
                 })}
                 onClick={() => setNext((p) => ({ ...p, object: "file" }))}
               >
@@ -237,9 +322,9 @@ export default function Web(props) {
               <button
                 className={cn({
                   "border hover:opacity-70 px-4 py-2 flex-1": true,
-                  "bg-blue-500 text-white border-blue-900 shadow-inner":
+                  "bg-green-500 text-white border-green-900 shadow-inner":
                     next.object === "node",
-                  "bg-white": next.object !== "node",
+                  "bg-white text-green-600": next.object !== "node",
                 })}
                 onClick={() => setNext((p) => ({ ...p, object: "node" }))}
               >
