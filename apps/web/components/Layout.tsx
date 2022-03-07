2babfd82d8ea93de1139502759a52a25e0b0cea4
Evan Conrad <evan@roomservice.dev>
2022-03-07 09:15:47 -0800

    add explorer

--- apps/web/components/Layout.tsx
+++ apps/web/components/Layout.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Layout(props: { children: any }) {
   return (
-    <div>
+    <div className="w-full">
       <div className="border-b">
         <nav className="px-4 pt-4 pb-4 mx-auto max-w-3xl flex flex-flex items-center justify-between">
           <Link href="/">
@@ -12,6 +12,12 @@ export default function Layout(props: { children: any }) {
           </Link>
 
           <div className="flex gap-1 items-center">
+            <a
+              href="/explorer"
+              className=" underline text-sm px-2 flex justify-between items-center p-1 rounded-sm hover:opacity-50 cursor-pointer"
+            >
+              explorer
+            </a>
             <a
               target={"_blank"}
               href="https://github.com/rebase-foundation/openmetagraph"
