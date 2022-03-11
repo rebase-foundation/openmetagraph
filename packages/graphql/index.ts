d0df960fd63fbabf34791cd4ea3ce247fefe4f9b
Evan Conrad <evan@roomservice.dev>
2022-03-11 15:29:32 -0800

    fix schema builder

--- packages/graphql/index.ts
+++ packages/graphql/index.ts
@@ -8,20 +8,21 @@ import { buildCreateAlias } from "./buildCreateAlias";
 // Generates a graphql schema for open meta graph
 export async function buildGraphqlSchema(hooks: Hooks, omgSchemas: string[]) {
   const query = await buildQuery(hooks, omgSchemas);
-  const createDocument = await buildCreateDocument(hooks, omgSchemas);
   const createSchema = buildCreateSchema(hooks);
   const createAlias = buildCreateAlias(hooks);
+
+  const fields = { createSchema, createAlias } as any;
+  if (omgSchemas.length > 0) {
+    const createDocument = await buildCreateDocument(hooks, omgSchemas);
+    fields.createDocument = createDocument;
+  }
 
   return new GraphQLSchema({
     ...query,
     mutation: new GraphQLObjectType({
       name: "Mutation",
 
-      fields: {
-        createDocument,
-        createSchema,
-        createAlias,
-      },
+      fields,
     }),
   });
 }
